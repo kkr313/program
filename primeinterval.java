4af5a02881289149b6586fe5fc92a1fb83113d58
kkr313 <kkr31398@gmail.com>
2017-07-14 18:12:57 +0530

    Create primeinterval.java

---
+++ primeinterval.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+class Prime
+{
+    public static void main(String args[])
+    {
+         int s1=20, s2=40,flag = 0, i, j;
+        
+         for(i = s1; i <= s2; i++)
+         {
+             for( j = 2; j < i; j++)
+             {
+                 if(i % j == 0)
+                 {
+                     flag = 0;
+                     break;
+                 }
+                 else
+                 {
+                     flag = 1;
+                 }
+             }
+             if(flag == 1)
+             {
+                 System.out.println(i);
+             }
+         }
+    }
+}
