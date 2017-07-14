12c924b2b7957b7762d6ae48c992a32b6e9f096e
kkr313 <kkr31398@gmail.com>
2017-07-14 17:54:08 +0530

    Create oddinterval.c

---
+++ oddinterval.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+void main()
+
+{
+
+    int num,m =20,n=40;
+
+    for (num = m; num <= n; num++)
+
+        {
+
+               if (num % 2 == 1)
+
+                  printf ("%d ", num);
+
+         }
+
+         
+
+} 
