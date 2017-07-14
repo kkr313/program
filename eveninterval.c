e6688b46ae91587cdfb92b24ebc16437527cfadd
kkr313 <kkr31398@gmail.com>
2017-07-14 17:58:32 +0530

    Create eveninterval.c

---
+++ eveninterval.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+
+{
+
+    int num,m =20,n=40;
+
+    for (num = m; num <= n; num++)
+
+        {
+
+               if (num % 2 == 0)
+
+                  printf ("%d ", num);
+
+         }
+
+
+} 
